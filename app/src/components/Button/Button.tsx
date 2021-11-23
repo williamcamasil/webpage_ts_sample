6d139c5ee44052346d42d63abebb291a678873d9
William Camargo <william.camargo@MacBook-Pro-de-administrador.local>
2021-11-23 10:50:33 -0300

    add folder and sample files

---
+++ app/src/components/Button/Button.tsx
@@ -0,0 +1,9 @@
+const Button = () => {
+    return (
+      <div>
+        <p>Button Component</p>
+      </div>
+    );
+  }
+  
+  export default Button;
--- app/src/screens/Home/Home.tsx
+++ app/src/screens/Home/Home.tsx
@@ -1,7 +1,10 @@
+import Button from '../../components/Button/Button';
+
 const Home = () => {
   return (
     <div>
       <p>Home Screen</p>
+      <Button />
     </div>
   );
 }
